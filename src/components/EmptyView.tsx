a4ca5f771501f6a9f6ba692aef2e8d40ce534d23
Mark de Vries <mark@lamalama.nl>
2023-02-11 21:29:11 +0100

    init extension

---
+++ src/components/EmptyView.tsx
@@ -0,0 +1,6 @@
+import { List } from "@raycast/api";
+
+function EmptyView() {
+  return <List.EmptyView icon="😕" title="No sites found" />;
+}
+export default EmptyView;
---
+++ src/sites.tsx
@@ -0,0 +1,29 @@
+import { Icon, List, ActionPanel, Action, getPreferenceValues } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { EmptyView } from "./components";
+
+export default function Command() {
+  const { API_TOKEN } = getPreferenceValues();
+
+  const { isLoading, data } = useFetch("https://ohdear.app/api/sites", {
+    headers: { Authorization: `Bearer ${API_TOKEN}` },
+  });
+
+  return (
+    <List isLoading={isLoading}>
+      <EmptyView />
+      {(data.data || []).map((site) => (
+        <List.Item
+          key={site.id}
+          icon={site.summarized_check_result === "succeeded" ? Icon.Checkmark : Icon.Circle}
+          title={site.label}
+          actions={
+            <ActionPanel title="#1 in raycast/extensions">
+              <Action.OpenInBrowser url={`https://ohdear.app/sites/${site.id}/active-checks`} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
